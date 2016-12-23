3c12af7c630e7de3c6578e042b33232f6529a23e
Majidhhdri <majid.hheidari@gmail.com>
2016-12-23 19:14:19 +0330

    Redesigned the app

--- app/src/main/java/com/majidhhdri/kheilisabz/dini/MainActivity.java
+++ app/src/main/java/com/majidhhdri/kheilisabz/dini/MainActivity.java
@@ -1,8 +1,10 @@
 package com.majidhhdri.kheilisabz.dini;
 
+import android.support.design.internal.BottomNavigationMenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         solution = (ImageView)findViewById(R.id.solution);
         int id = getApplicationContext().getResources().getIdentifier("dini0001", "drawable", getPackageName());
         solution.setImageResource(id);
